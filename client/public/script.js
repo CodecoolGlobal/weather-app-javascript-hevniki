080237f986335cfb94a28d76a9eec0d76efc6a9a
Nikolett Hevizi <nikihevizi@gmail.com>
2023-06-07 12:56:01 +0200

    create background

--- client/public/script.js
+++ client/public/script.js
@@ -60,48 +60,38 @@ async function fetchCityFromPexelAPI(city){
   return data;
 }
 
-// function applyWeatherEffects(data) {
-//   const bodyElement = document.querySelector('body');
-//   const weatherDiv = document.querySelector('.weather-widget');
+function applyWeatherEffects(weatherData) {
+  const bodyElement = document.querySelector('body');
+  const videoElement = document.createElement('video');
 
-//   if (weatherDiv.classList.contains('hotDay')) {
-//     const videoElement = document.createElement('video');
-//     videoElement.src = './public/hotday.mp4';
-//     videoElement.autoplay = true;
-//     videoElement.loop = true;
-//     videoElement.muted = true;
-//     videoElement.classList.add('fullscreen-video');
+  if (weatherData.current.temp_c > 20 && weatherData.current.is_day !== 0) {
+    videoElement.src = './public/hotday.mp4';
+    videoElement.autoplay = true;
+    videoElement.loop = true;
+    videoElement.muted = true;
+    videoElement.classList.add('fullscreen-video');
+  } else if (weatherData.current.temp_c > 20 && weatherData.current.is_day === 0) {
+    videoElement.src = './public/hotnight.mp4';
+    videoElement.autoplay = true;
+    videoElement.loop = true;
+    videoElement.muted = true;
+    videoElement.classList.add('fullscreen-video');
+  }else if (weatherData.current.temp_c < 20 && weatherData.current.is_day !== 1) {
+    videoElement.src = './public/coldday.mp4';
+    videoElement.autoplay = true;
+    videoElement.loop = true;
+    videoElement.muted = true;
+    videoElement.classList.add('fullscreen-video');
+  }else if (weatherData.current.temp_c < 20 && weatherData.current.is_day === 1) {
+  videoElement.src = './public/coldnight.mp4';
+  videoElement.autoplay = true;
+  videoElement.loop = true;
+  videoElement.muted = true;
+  videoElement.classList.add('fullscreen-video');
+  }
+  bodyElement.appendChild(videoElement);
+}
 
-//     bodyElement.appendChild(videoElement);
-//   } else if (weatherDiv.classList.contains('coldDay')){
-//     const videoElement = document.createElement('video');
-//     videoElement.src = './public/coldday.mp4';
-//     videoElement.autoplay = true;
-//     videoElement.loop = true;
-//     videoElement.muted = true;
-//     videoElement.classList.add('fullscreen-video');
-
-//     bodyElement.appendChild(videoElement);
-// } else if (weatherDiv.classList.contains('hotNight')){
-//   const videoElement = document.createElement('video');
-//   videoElement.src = './public/hotnight.mp4';
-//   videoElement.autoplay = true;
-//   videoElement.loop = true;
-//   videoElement.muted = true;
-//   videoElement.classList.add('fullscreen-video');
-
-//   bodyElement.appendChild(videoElement);
-// } else if (weatherDiv.classList.contains('coldNight')){
-//   const videoElement = document.createElement('video');
-//   videoElement.src = './public/coldnight.mp4';
-//   videoElement.autoplay = true;
-//   videoElement.loop = true;
-//   videoElement.muted = true;
-//   videoElement.classList.add('fullscreen-video');
-
-//   bodyElement.appendChild(videoElement);
-// }
-// }
 
 function listDetailsOnPage(weatherData, pexelData) {
 rootElement.innerText = '';
@@ -112,8 +102,8 @@ currentDiv.id = 'id_current';
 currentDiv.classList.add('container')
 
 const currDiv1 = document.createElement('div')
-const cityNameElem = document.createElement('h1')
-const countryNameElem = document.createElement('h2')
+const cityNameElem = document.createElement('h2')
+const countryNameElem = document.createElement('h3')
 cityNameElem.innerText = weatherData.location.name;
 countryNameElem.innerText = weatherData.location.country;
 
@@ -130,42 +120,43 @@ currDiv2.classList.add('widget')
 
 const currDiv3 = document.createElement('div')
 const condText = document.createElement('h1');
-condText.innerText = weatherData.current.condition['text']
+condText.innerText = `${weatherData.current.condition['text']} \n\n`
 const condImg = document.createElement('img');
 condImg.src = weatherData.current.condition.icon;
+condImg.classList.add('icon')
 
 currDiv3.insertAdjacentElement('beforeend', condText)
 currDiv3.insertAdjacentElement('beforeend', condImg)
 currDiv3.classList.add('widget')
 
 const currDiv4 = document.createElement('div')
-const currTemperature= document.createElement('h1')
-const feelsLike = document.createElement('h1')
-currTemperature.innerText = weatherData.current.temp_c;
-feelsLike.innerText = weatherData.current.feelslike_c;
+const currTemperature= document.createElement('h2')
+const feelsLike = document.createElement('h2')
+currTemperature.innerText = `Temperature: \n ${weatherData.current.temp_c} °C`;
+feelsLike.innerText = `\n Feels like: \n ${weatherData.current.feelslike_c} °C`;
 
 currDiv4.insertAdjacentElement('beforeend', currTemperature);
 currDiv4.insertAdjacentElement('beforeend', feelsLike);
 currDiv4.classList.add('widget')
 
 const currDiv5 = document.createElement('div')
-const humidity= document.createElement('h1')
-const wind = document.createElement('h1')
-const windDir = document.createElement('h1')
-humidity.innerText = weatherData.current.humidity;
-wind.innerText = weatherData.current.wind_kph;
-windDir.innerText = weatherData.current.wind_dir;
+const humidity= document.createElement('h2')
+const wind = document.createElement('h2')
+
+humidity.innerText = `Humidity: \n ${weatherData.current.humidity} %`;
+wind.innerText = ` \n Wind: \n ${weatherData.current.wind_kph} km/h`;
+
 
 currDiv5.insertAdjacentElement('beforeend', humidity);
 currDiv5.insertAdjacentElement('beforeend', wind);
-currDiv5.insertAdjacentElement('beforeend', windDir);
+
 currDiv5.classList.add('widget')
 
 const currDiv6 = document.createElement('div')
-const dailyMin= document.createElement('h1')
-const dailyMax = document.createElement('h1')
-dailyMin.innerText = weatherData.forecast.forecastday[0].day.mintemp_c
-dailyMax.innerText = weatherData.forecast.forecastday[0].day.maxtemp_c
+const dailyMin= document.createElement('h2')
+const dailyMax = document.createElement('h2')
+dailyMin.innerText = `Min temp: \n ${weatherData.forecast.forecastday[0].day.mintemp_c} °C`
+dailyMax.innerText = `\n Max temp: \n ${weatherData.forecast.forecastday[0].day.maxtemp_c} °C`
 
 currDiv6.insertAdjacentElement('beforeend', dailyMin);
 currDiv6.insertAdjacentElement('beforeend', dailyMax);
@@ -180,7 +171,6 @@ currentDiv.insertAdjacentElement('beforeend', currDiv6)
 
 rootElement.insertAdjacentElement('beforeend', currentDiv)
 
-
 if(pexelData.photos.length > 0){
   const max = pexelData.photos.length - 1;
   const random = Math.round(Math.random() * max);
@@ -190,29 +180,9 @@ if(pexelData.photos.length > 0){
   currDiv1.insertAdjacentElement('beforeend', cityPicElem);
   }
 
-
-
-
   console.log(weatherData);
-  const dayTime = {};
-  if(weatherData.current.is_day !== 1){
-    dayTime.innerText = `Night`
-  }
-  else{
-    dayTime.innerText = `Day`
-  }
 
-  if (weatherData.current.temp_c > 20 && weatherData.current.is_day !== 0) {
-    currentDiv.classList.add('hotDay');
-  } else if (weatherData.current.temp_c > 20 && weatherData.current.is_day === 0) {
-    currentDiv.classList.add('hotNight');
-  }else if (weatherData.current.temp_c < 20 && weatherData.current.is_day !== 1) {
-    currentDiv.classList.add('coldDay');
-  }else if (weatherData.current.temp_c < 20 && weatherData.current.is_day === 1) {
-      currentDiv.classList.add('coldNight');
-  }
-
-  //applyWeatherEffects(weatherData);
+  applyWeatherEffects(weatherData);
 
 }
 
@@ -220,6 +190,9 @@ if(pexelData.photos.length > 0){
 
 const loadEvent = function () {
   createInputField();
+  Promise.all([fetchCityFromWeatherAPI('budapest'), fetchCityFromPexelAPI('budapest')]).then((values)=>{
+    listDetailsOnPage(values[0], values[1]);
+  });
 };
 
 window.addEventListener('load', loadEvent);
